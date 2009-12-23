872078aa1f7b86096874ecbe3bbead073f1149b4
aegypius <innercircle@aegypius.com>
2009-12-23 20:06:14 +0100

    Updating feed library

--- libraries/Feed.php
+++ libraries/Feed.php
@@ -3,11 +3,37 @@
 class Feed {
 
 	protected $encoding = 'utf-8';
-	protected $url = null;
+	protected $url;
+	protected $data;
 
 	public function Feed($url = null) {
-		if (is_string($url) && strlen($url))
+		if (is_string($url) && strlen($url)) {
 			$this->url = trim($url);
+			$this->request();
+		}
+	}
+
+	protected function request() {
+
+		// curl initialization
+		$ch = curl_init();
+
+		curl_setopt($ch, CURLOPT_URL, $this->url);
+		curl_setopt($ch, CURLOPT_TIMEOUT, 3);
+
+		curl_setopt($ch, CURLOPT_HTTPHEADERS,array('Content-Type: text/xml'));
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+
+		$content = curl_exec($ch);
+
+		if (curl_errno($ch) != CURLE_OK) {
+			$errno = curl_errno($ch);
+			$error = curl_error($ch);
+			curl_close($ch);
+		}
+
+		return $this->data = $content;
 	}
 
 }
