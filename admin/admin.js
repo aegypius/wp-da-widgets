7c2a268e256468e630da618ab6c323f9722d3661
aegypius <aegypius@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-02-16 16:04:31 +0000

    Refactoring admin to add custom css support
    git-svn-id: http://plugins.svn.wordpress.org/deviantart-widgets/trunk@206522 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/admin.js
+++ admin/admin.js
@@ -10,6 +10,7 @@ jQuery(document).ready(function() {
 			jQuery('#' + v ).attr('disabled', !enabled);
 		});
 	});
+
 	jQuery('#thumb-enabled').click(function() {
 		var enabled = this.checked;
 		jQuery.each(['thumb-path', 'thumb-size-x', 'thumb-size-y', 'thumb-format'], function(k, v) {
@@ -20,6 +21,34 @@ jQuery(document).ready(function() {
 			}
 			jQuery('#' + v ).attr('disabled', !enabled);
 		});
-
 	});
+
+	function Tabpanel () {
+		this.__construct.apply(this, arguments);
+	};
+	Tabpanel.prototype = {
+		cache: [],
+		__construct: function(selector) {
+			jQuery(selector).addClass('tabs');
+			this.cache = jQuery(selector + ' > ul > li > a');
+			this.bindEvents();
+			jQuery(this.cache[0]).trigger('click');
+		},
+		bindEvents: function() {
+			var links = this.cache;
+			jQuery.each(links, function(idx, tab) {
+				jQuery(tab).click(function(event) {
+					jQuery.each(links, function(idx, e) {
+						jQuery(jQuery(e).attr('href')).hide()
+						jQuery(e).parent().removeClass('active')
+					});
+					jQuery(jQuery(this).attr('href')).show();
+					jQuery(this).parent().addClass('active');
+					return false;
+				});
+			});
+		}
+	};
+
+	var t = new Tabpanel('#da-widgets-settings');
 });
--- admin/admin.php
+++ admin/admin.php
@@ -26,9 +26,10 @@ function da_widgets_admin_meta($links, $file) {
 }
 
 function da_widgets_admin_head() {
-	if (isset($_GET['page']) && $_GET['page'] == DA_WIDGETS_ADMIN_PAGE) {
-		echo "<link rel='stylesheet' type='text/css' href='" . compat_get_base_plugin_url() .'/'. dirname(DA_WIDGETS_ADMIN_PAGE) . "/theme.css' />\n";
-		echo "<script type='text/javascript' src='" . compat_get_base_plugin_url() .'/'. dirname(DA_WIDGETS_ADMIN_PAGE) . "/admin.js' />\n";
+	if (isset($_GET['page']) && $_GET['page'] == DA_WIDGETS_ADMIN_PAGE) {?>
+		<link rel="stylesheet" type="text/css" href="<?php echo compat_get_base_plugin_url()?>/<?php echo dirname(DA_WIDGETS_ADMIN_PAGE)?>/theme.css" />
+		<script type="text/javascript"          src="<?php echo compat_get_base_plugin_url()?>/<?php echo dirname(DA_WIDGETS_ADMIN_PAGE)?>/admin.js"></script>
+	<?php
 	}
 }
 
@@ -58,6 +59,9 @@ function da_widgets_admin_settings() {
 
 	// Advanced Settings
 	register_setting('da-widgets-settings', 'debug-enabled', 'intval');
+
+	// CSS Customization
+	register_setting('da-widgets-settings', 'user-css', 'trim');
 
 	// Adding Cache cleaning
 	register_setting('da-widgets-settings', 'empty-cache', 'da_widgets_admin_clean_cache' );
@@ -135,63 +139,69 @@ function da_widgets_admin_page() {
 	<h2><?php _e('deviantArt Widgets Settings', 'da-widgets') ?></h2>
 	<p id="da-widgets-version"> version <?php echo DA_Widgets::VERSION ?></p>
 
+	<ul>
+		<li class="tab"><a href="#general"><?php _e('General', 'da-widgets')?></a></li>
+		<li class="tab"><a href="#customization"><?php _e('Customization', 'da-widgets')?></a></li>
+	</ul>
+
 	<form action="options.php" method="post">
 		<?php wp_nonce_field('update-options'); ?>
 		<?php settings_fields('da-widgets-settings'); ?>
-		<fieldset>
-			<legend><?php _e('Cache Settings', 'da-widgets') ?></legend>
-			<p><?php _e('Defines whether you want to use cache or not.', 'da-widgets')?></p>
-			<dl>
-				<dt><label for="cache-enabled"><?php _e('Enable cache', 'da-widgets') ?></label></dt>
-				<dd><input <?php echo get_option('cache-enabled') ? 'checked="checked"' : '' ?> type="checkbox" id="cache-enabled" name="cache-enabled" value="1"/></dd>
-				<dt><label <?php echo !get_option('cache-enabled') ? 'class="disabled"' : '' ?> for="cache-duration"><?php _e('Cache duration', 'da-widgets') ?></label></dt>
-				<dd>
-					<select <?php echo !get_option('cache-enabled') ? 'disabled="disabled"' : '' ?> id="cache-duration" name="cache-duration">
-						<option <?php echo selected(get_option('cache-duration'),   15)?> value="15">15 <?php _e('minutes', 'da-widgets')?> (default)</option>
-						<option <?php echo selected(get_option('cache-duration'),   30)?> value="30">30 <?php _e('minutes', 'da-widgets')?></option>
-						<option <?php echo selected(get_option('cache-duration'),   60)?> value="60">1 <?php _e('hour', 'da-widgets')?></option>
-						<option <?php echo selected(get_option('cache-duration'),  120)?> value="120">2 <?php _e('hour', 'da-widgets')?></option>
-						<option <?php echo selected(get_option('cache-duration'),  180)?> value="180">3 <?php _e('hour', 'da-widgets')?></option>
-						<option <?php echo selected(get_option('cache-duration'), 1440)?> value="1440">1 <?php _e('day', 'da-widgets')?></option>
-					</select>
-				</dd>
-			</dl>
-		</fieldset>
-		<fieldset>
-			<legend><?php _e('Thumbnails', 'da-widgets') ?></legend>
-			<p><?php _e('Defines thumbnails properties.', 'da-widgets')?></p>
-			<dl>
-				<dt><label for="thumb-enabled"><?php _e('Thumbnail generation', 'da-widgets') ?></label></dt>
-				<dd><input <?php echo get_option('thumb-enabled') ? 'checked="checked"' : '' ?> type="checkbox" id="thumb-enabled" name="thumb-enabled" value="1"/></dd>
 
-				<dt><label <?php echo !get_option('thumb-enabled') ? 'class="disabled"' : '' ?> for="thumb-size-x"><?php _e('Thumbnail size', 'da-widgets')?></label></dt>
-				<dd>
-					<input <?php echo !get_option('thumb-enabled') ? 'disabled="disabled"' : '' ?> id="thumb-size-x" name="thumb-size-x" type="text" size="3" maxlength="3" value="<?php echo get_option('thumb-size-x')?>" />
-					x
-					<input <?php echo !get_option('thumb-enabled') ? 'disabled="disabled"' : '' ?> id="thumb-size-y" name="thumb-size-y" type="text" size="3" maxlength="3" value="<?php echo get_option('thumb-size-y')?>" />
-				</dd>
+		<div id="general" class="tab">
+			<fieldset>
+				<legend><?php _e('Cache Settings', 'da-widgets') ?></legend>
+				<p><?php _e('Defines whether you want to use cache or not.', 'da-widgets')?></p>
+				<dl>
+					<dt><label for="cache-enabled"><?php _e('Enable cache', 'da-widgets') ?></label></dt>
+					<dd><input <?php echo get_option('cache-enabled') ? 'checked="checked"' : '' ?> type="checkbox" id="cache-enabled" name="cache-enabled" value="1"/></dd>
+					<dt><label <?php echo !get_option('cache-enabled') ? 'class="disabled"' : '' ?> for="cache-duration"><?php _e('Cache duration', 'da-widgets') ?></label></dt>
+					<dd>
+						<select <?php echo !get_option('cache-enabled') ? 'disabled="disabled"' : '' ?> id="cache-duration" name="cache-duration">
+							<option <?php selected(get_option('cache-duration'),   15)?> value="15">15 <?php _e('minutes', 'da-widgets')?> (default)</option>
+							<option <?php selected(get_option('cache-duration'),   30)?> value="30">30 <?php _e('minutes', 'da-widgets')?></option>
+							<option <?php selected(get_option('cache-duration'),   60)?> value="60">1 <?php _e('hour', 'da-widgets')?></option>
+							<option <?php selected(get_option('cache-duration'),  120)?> value="120">2 <?php _e('hour', 'da-widgets')?></option>
+							<option <?php selected(get_option('cache-duration'),  180)?> value="180">3 <?php _e('hour', 'da-widgets')?></option>
+							<option <?php selected(get_option('cache-duration'), 1440)?> value="1440">1 <?php _e('day', 'da-widgets')?></option>
+						</select>
+					</dd>
+				</dl>
+			</fieldset>
+			<fieldset>
+				<legend><?php _e('Thumbnails', 'da-widgets') ?></legend>
+				<p><?php _e('Defines thumbnails properties.', 'da-widgets')?></p>
+				<dl>
+					<dt><label for="thumb-enabled"><?php _e('Thumbnail generation', 'da-widgets') ?></label></dt>
+					<dd><input <?php echo get_option('thumb-enabled') ? 'checked="checked"' : '' ?> type="checkbox" id="thumb-enabled" name="thumb-enabled" value="1"/></dd>
 
-				<dt><label <?php echo !get_option('thumb-enabled') ? 'class="disabled"' : '' ?> for="thumb-format"><?php _e('Thumbnail format', 'da-widgets') ?></label></dt>
-				<dd>
-					<select <?php echo !get_option('thumb-enabled') ? 'disabled="disabled"' : '' ?> id="thumb-format" name="thumb-format">
-						<option <?php echo selected(get_option('thumb-format'), IMG_PNG)?> value="<?php echo IMG_PNG?>">PNG (default)</option>
-						<option <?php echo selected(get_option('thumb-format'), IMG_JPG)?> value="<?php echo IMG_JPG?>">JPEG</option>
-						<option <?php echo selected(get_option('thumb-format'), IMG_GIF)?> value="<?php echo IMG_GIF?>">GIF</option>
-					</select>
-				</dd>
-			</dl>
-		</fieldset>
-		<fieldset>
-			<legend><?php _e('Advanced options', 'da-widgets') ?></legend>
-			<p><?php _e('Defines options to advanced features.', 'da-widgets')?></p>
-			<dl>
-				<dt><label for="debug-enabled"><? _e('Enable debug log', 'da-widgets')?></label></dt>
-				<dd><input <?php echo get_option('debug-enabled') ? 'checked="checked"' : '' ?> type="checkbox" id="debug-enabled" name="debug-enabled" value="1"/></dd>
-			</dl>
-		</fieldset>
-		<input type="submit" value="<?php _e('Save', 'da-widgets')?>" class="button-primary" />
-		<input class="button" type="submit" value="<?php _e('Empty cache', 'da-widgets')?>" name="empty-cache" />
-	</form>
+					<dt><label <?php echo !get_option('thumb-enabled') ? 'class="disabled"' : '' ?> for="thumb-size-x"><?php _e('Thumbnail size', 'da-widgets')?></label></dt>
+					<dd>
+						<input <?php echo !get_option('thumb-enabled') ? 'disabled="disabled"' : '' ?> id="thumb-size-x" name="thumb-size-x" type="text" size="3" maxlength="3" value="<?php echo get_option('thumb-size-x')?>" />
+						x
+						<input <?php echo !get_option('thumb-enabled') ? 'disabled="disabled"' : '' ?> id="thumb-size-y" name="thumb-size-y" type="text" size="3" maxlength="3" value="<?php echo get_option('thumb-size-y')?>" />
+					</dd>
+
+					<dt><label <?php echo !get_option('thumb-enabled') ? 'class="disabled"' : '' ?> for="thumb-format"><?php _e('Thumbnail format', 'da-widgets') ?></label></dt>
+					<dd>
+						<select <?php echo !get_option('thumb-enabled') ? 'disabled="disabled"' : '' ?> id="thumb-format" name="thumb-format">
+							<option <?php selected(get_option('thumb-format'), IMG_PNG)?> value="<?php echo IMG_PNG?>">PNG (default)</option>
+							<option <?php selected(get_option('thumb-format'), IMG_JPG)?> value="<?php echo IMG_JPG?>">JPEG</option>
+							<option <?php selected(get_option('thumb-format'), IMG_GIF)?> value="<?php echo IMG_GIF?>">GIF</option>
+						</select>
+					</dd>
+				</dl>
+			</fieldset>
+			<fieldset>
+				<legend><?php _e('Advanced options', 'da-widgets') ?></legend>
+				<p><?php _e('Defines options to advanced features.', 'da-widgets')?></p>
+				<dl>
+					<dt><label for="debug-enabled"><? _e('Enable debug log', 'da-widgets')?></label></dt>
+					<dd><input <?php echo get_option('debug-enabled') ? 'checked="checked"' : '' ?> type="checkbox" id="debug-enabled" name="debug-enabled" value="1"/></dd>
+				</dl>
+			</fieldset>
+			<input type="submit" value="<?php _e('Save', 'da-widgets')?>" class="button-primary" />
+			<input class="button" type="submit" value="<?php _e('Empty cache', 'da-widgets')?>" name="empty-cache" />
 
 <?php
 	$error_message = array(
@@ -207,9 +217,9 @@ function da_widgets_admin_page() {
 		512  => sprintf(__('No suitable method to process remote images for this plugin (allow_url_fopen is disabled, curl is required)', 'da-widgets'))
 	);
 ?>
-	<fieldset>
-		<legend><?php _e('Setup checks', 'da-widgets')?></legend>
-		<ul>
+			<fieldset>
+				<legend><?php _e('Setup checks', 'da-widgets')?></legend>
+				<ul>
 <?php
 
 	if (($failures = da_widgets_admin_check()) > 0) {
@@ -222,8 +232,48 @@ function da_widgets_admin_page() {
 		printf('<li class="message">%s.</li>', __('Everything is fine', 'da-widgets'));
 	}
 ?>
-		</ul>
-	</fieldset>
+				</ul>
+			</fieldset>
+		</div>
+
+		<div id="customization" class="tab">
+			<fieldset>
+				<legend><?php _e('Style customization', 'da-widgets')?></legend>
+				<dl class="horizontal">
+					<dt><label for="user-css"><?php _e('Custom Styles', 'da-widgets')?></label></dt>
+					<dd><textarea id="user-css" class="source" rows="10" cols="72" name="user-css"><?php echo get_option('user-css')?></textarea></dd>
+				</dl>
+
+				<p>
+					<ul>
+						<li><? _e('Galleries', 'da-widgets')?> : 
+				<pre>
+&lt;ul class="da-widgets gallery"&gt;
+	&lt;li&gt;&lt;a [...]&gt;&lt;img [...]&gt;&lt;/a&gt;&lt;/li&gt;
+	&lt;li&gt;&lt;a [...]&gt;&lt;img [...]&gt;&lt;/a&gt;&lt;/li&gt;
+&lt;/ul&gt;</pre>
+						</li>
+						<li><? _e('Favourites', 'da-widgets')?> : 
+				<pre>
+&lt;ul class="da-widgets favourite"&gt;
+	&lt;li&gt;&lt;a [...]&gt;&lt;img [...]&gt;&lt;/a&gt;&lt;/li&gt;
+	&lt;li&gt;&lt;a [...]&gt;&lt;img [...]&gt;&lt;/a&gt;&lt;/li&gt;
+&lt;/ul&gt;</pre>
+						</li>
+						<li><? _e('Logs', 'da-widgets')?> : 
+				<pre>
+&lt;dl class="da-widgets log"&gt;
+	&lt;dt&gt;&lt;a [...]&gt;{title}&lt;/a&gt;&lt;/dt&gt;
+	&lt;dd&gt;&lt;p&gt;{content}&lt;p&gt;&lt;/dd&gt;
+&lt;/dl&gt;</pre>
+						</li>
+					</ul>
+				</p>
+				<input type="submit" value="<?php _e('Save', 'da-widgets')?>" class="button-primary" />
+			</fieldset>
+		</div>
+
+	</form>
 </div>
 <?
 }
