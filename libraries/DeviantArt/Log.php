1a7bd9bcba62e07cb6c6350613091823f96bfccd
aegypius <aegypius@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-02-24 13:40:56 +0000

    Adding Category filtering for galleries and favourites
    git-svn-id: http://plugins.svn.wordpress.org/deviantart-widgets/trunk@209797 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- libraries/DeviantArt/Log.php
+++ libraries/DeviantArt/Log.php
@@ -4,14 +4,19 @@ require_once realpath(dirname(__FILE__) . '/..') . '/Feed.php';
 class DeviantArt_Log extends Feed {
 
 	const BACKEND_URL = 'http://backend.deviantart.com/rss.xml?q=by%%3A%s&type=journal&formatted=%d';
+	protected $username;
 
 	public function DeviantArt_Log($username, $format=1) {
-		$url = sprintf(self::BACKEND_URL, $username, $format);
-		parent::Feed($url);
+		$this->username = trim($username);
+		parent::Feed();
 	}
 
-	public function get($count = -1) {
+	public function get($count = -1, $format = 1) {
 		if ($count == 0 || !is_numeric($count)) $count = -1;
+
+		$url = sprintf(self::BACKEND_URL, $this->username, $format);
+
+		$this->data = $this->request($url);
 
 		$xml = new SimpleXmlElement($this->data);
 		$ns = $xml->getNamespaces(true);
