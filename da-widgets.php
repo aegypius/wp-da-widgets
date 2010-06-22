4f2a0dd8fdc8393926a6f59eeaa9838db499b671
aegypius <innercircle@aegypius.com>
2010-06-22 12:37:08 +0200

    Clean html widget form from main widget class to a separated include

--- da-widgets.php
+++ da-widgets.php
@@ -60,79 +60,8 @@ if (class_exists('WP_Widget')) {
 			$html		= intval($instance['html']);
 			$filter		= intval($instance['filter']);
 
-	?>
-		<p>
-			<label for="<?php echo $this->get_field_id('title')?>"><?php _e('Title', 'da-widgets')?> : </label>
-			<input class="widefat" type="text" id="<?php echo $this->get_field_id('title')?>" name="<?php echo $this->get_field_name('title')?>" value="<?php echo $title?>" />
-		</p>
+			include 'includes/templates/widget_form.php';
 
-		<p>
-			<label for="<?php echo $this->get_field_id('type')?>"><?php _e('Content', 'da-widgets')?> : </label>
-			<select class="widefat" id="<?php echo $this->get_field_id('type')?>" name="<?php echo $this->get_field_name('type')?>">
-				<option <?php selected(self::DA_WIDGET_LOG, $type); ?> value="<?php echo self::DA_WIDGET_LOG?>">Journal</option>
-				<option <?php selected(self::DA_WIDGET_GALLERY, $type); ?> value="<?php echo self::DA_WIDGET_GALLERY?>">Gallery</option>
-				<option <?php selected(self::DA_WIDGET_FAVOURITE, $type); ?> value="<?php echo self::DA_WIDGET_FAVOURITE?>">Favourites</option>
-			</select>
-		</p>
-
-		<p>
-			<label for="<?php echo $this->get_field_id('deviant')?>"><?php _e('Deviant', 'da-widgets')?> : </label>
-			<input class="widefat" type="text" id="<?php echo $this->get_field_id('deviant')?>" name="<?php echo $this->get_field_name('deviant')?>" value="<?php echo $deviant?>" />
-		</p>
-
-		<p>
-			<label for="<?php echo $this->get_field_id('items')?>"><?php _e('Items to display', 'da-widgets')?> : </label>
-			<select class="widefat" id="<?php echo $this->get_field_id('items')?>" name="<?php echo $this->get_field_name('items')?>">
-
-				<option <?php selected(-1 , $items) ?> value="-1"><?php _e('All', 'da-widgets')?></option>
-
-			<?php foreach (range(1,10) as $v) : ?>
-				<option <?php selected($v , $items) ?> value="<?php echo $v?>"><?php echo $v?></option>
-			<?php endforeach; ?>
-			</select>
-		</p>
-
-		<?php if ($type == self::DA_WIDGET_LOG) : ?>
-		<p>
-			<input class="checkbox" type="checkbox" id="<?php echo $this->get_field_id('html')?>" name="<?php echo $this->get_field_name('html')?>" value="1" <?php if ( $html ) { echo 'checked="checked"'; } ?>/>
-			<label for="<?php echo $this->get_field_id('html')?>"><?php _e('Keep original formating', 'da-widgets')?></label>
-		</p>
-		<?php else : ?>
-		<p>
-			<label for="<?php echo $this->get_field_id('rating')?>"><?php _e('Content Rating', 'da-widgets')?> : </label>
-			<select class="widefat" id="<?php echo $this->get_field_id('rating')?>" name="<?php echo $this->get_field_name('rating')?>">
-				<option <?php selected('nonadult', $rating); ?> value="nonadult"><?php _e('Forbid adult content', 'da-widgets')?></option>
-				<option <?php selected('all', $rating); ?> value="all"><?php _e('Allow adult content', 'da-widgets')?></option>
-			</select>
-		</p>
-<?php
-			switch ($type) {
-				case self::DA_WIDGET_GALLERY:
-					$res = new DeviantArt_Gallery($deviant, $rating, $scraps);
-					break;
-				default:
-				case self::DA_WIDGET_FAVOURITE:
-					$res = new DeviantArt_Favourite($deviant, $rating, $scraps);
-					break;
-			}
-
-			foreach ($res->getCategories() as $categoryId=>$categoryName) {
-				$options .= sprintf('<option value="%s"%s>%s</option>', $categoryId, ($categoryId == $filter ? ' selected="selected"' : ''), $categoryName);
-			}
-?>
-		<p>
-			<label for="<?php echo $this->get_field_id('filter')?>"><?php _e('Category filter', 'da-widgets')?> : </label>
-			<select class="widefat" id="<?php echo $this->get_field_id('filter')?>" name="<?php echo $this->get_field_name('filter')?>">
-				<option <?php selected(0, $filter); ?> value="0"><?php _e('Disabled', 'da-widgets')?></option>
-				<?php echo $options ?>
-				<option <?php selected(-1, $filter); ?> value="-1"><?php _e('Scraps', 'da-widgets')?></option>
-			</select>
-		</p>
-
-		<?php endif; ?>
-
-
-	<?php
 		}
 
 		function css() {
@@ -449,6 +378,15 @@ if (class_exists('WP_Widget')) {
 			wp_register_style('da-widgets', PLUGIN_URL . '/public/css/style.css', array('slimbox2'), self::VERSION);
 			wp_enqueue_style('da-widgets');
 		}
+	
+		public static function shortcode_dependencies($posts) {
+			if (empty($posts)) return $posts;
+			$found = false;
+			foreach ($posts as $post) {
+					
+			}
+			return $posts;
+		}
 	}
 
 	// Register Widget
@@ -466,6 +404,9 @@ if (class_exists('WP_Widget')) {
 
 	// Setup I18n
 	load_plugin_textdomain('da-widgets', PLUGIN_ROOT, basename(dirname(__FILE__)));
+
+	// Filters for js/css inclusions
+	add_filter('the_posts', array('DA_Widgets', 'shortcode_dependencies'));
 
 	require_once realpath(dirname(__FILE__)).'/admin/admin.php';
 }
---
+++ includes/templates/widget_form.php
@@ -0,0 +1,69 @@
+	<p>
+		<label for="<?php echo $this->get_field_id('title')?>"><?php _e('Title', 'da-widgets')?> : </label>
+		<input class="widefat" type="text" id="<?php echo $this->get_field_id('title')?>" name="<?php echo $this->get_field_name('title')?>" value="<?php echo $title?>" />
+	</p>
+
+	<p>
+		<label for="<?php echo $this->get_field_id('type')?>"><?php _e('Content', 'da-widgets')?> : </label>
+		<select class="widefat" id="<?php echo $this->get_field_id('type')?>" name="<?php echo $this->get_field_name('type')?>">
+			<option <?php selected(self::DA_WIDGET_LOG, $type); ?> value="<?php echo self::DA_WIDGET_LOG?>"><? _e('Journal', 'da-widgets')?></option>
+			<option <?php selected(self::DA_WIDGET_GALLERY, $type); ?> value="<?php echo self::DA_WIDGET_GALLERY?>"><? _e('Gallery', 'da-widgets')?></option>
+			<option <?php selected(self::DA_WIDGET_FAVOURITE, $type); ?> value="<?php echo self::DA_WIDGET_FAVOURITE?>"><? _e('Favourites', 'da-widgets')?></option>
+		</select>
+	</p>
+
+	<p>
+		<label for="<?php echo $this->get_field_id('deviant')?>"><?php _e('Deviant', 'da-widgets')?> : </label>
+		<input class="widefat" type="text" id="<?php echo $this->get_field_id('deviant')?>" name="<?php echo $this->get_field_name('deviant')?>" value="<?php echo $deviant?>" />
+	</p>
+
+	<p>
+		<label for="<?php echo $this->get_field_id('items')?>"><?php _e('Items to display', 'da-widgets')?> : </label>
+		<select class="widefat" id="<?php echo $this->get_field_id('items')?>" name="<?php echo $this->get_field_name('items')?>">
+
+			<option <?php selected(-1 , $items) ?> value="-1"><?php _e('All', 'da-widgets')?></option>
+
+		<?php foreach (range(1,10) as $v) : ?>
+			<option <?php selected($v , $items) ?> value="<?php echo $v?>"><?php echo $v?></option>
+		<?php endforeach; ?>
+		</select>
+	</p>
+
+	<?php if ($type == self::DA_WIDGET_LOG) : ?>
+	<p>
+		<input class="checkbox" type="checkbox" id="<?php echo $this->get_field_id('html')?>" name="<?php echo $this->get_field_name('html')?>" value="1" <?php if ( $html ) { echo 'checked="checked"'; } ?>/>
+		<label for="<?php echo $this->get_field_id('html')?>"><?php _e('Keep original formating', 'da-widgets')?></label>
+	</p>
+	<?php else : ?>
+	<p>
+		<label for="<?php echo $this->get_field_id('rating')?>"><?php _e('Content Rating', 'da-widgets')?> : </label>
+		<select class="widefat" id="<?php echo $this->get_field_id('rating')?>" name="<?php echo $this->get_field_name('rating')?>">
+			<option <?php selected('nonadult', $rating); ?> value="nonadult"><?php _e('Forbid adult content', 'da-widgets')?></option>
+			<option <?php selected('all', $rating); ?> value="all"><?php _e('Allow adult content', 'da-widgets')?></option>
+		</select>
+	</p>
+<?php
+		switch ($type) {
+			case self::DA_WIDGET_GALLERY:
+				$res = new DeviantArt_Gallery($deviant, $rating, $scraps);
+				break;
+			default:
+			case self::DA_WIDGET_FAVOURITE:
+				$res = new DeviantArt_Favourite($deviant, $rating, $scraps);
+				break;
+		}
+
+		foreach ($res->getCategories() as $categoryId=>$categoryName) {
+			$options .= sprintf('<option value="%s"%s>%s</option>', $categoryId, ($categoryId == $filter ? ' selected="selected"' : ''), $categoryName);
+		}
+?>
+	<p>
+		<label for="<?php echo $this->get_field_id('filter')?>"><?php _e('Category filter', 'da-widgets')?> : </label>
+		<select class="widefat" id="<?php echo $this->get_field_id('filter')?>" name="<?php echo $this->get_field_name('filter')?>">
+			<option <?php selected(0, $filter); ?> value="0"><?php _e('Disabled', 'da-widgets')?></option>
+			<?php echo $options ?>
+			<option <?php selected(-1, $filter); ?> value="-1"><?php _e('Scraps', 'da-widgets')?></option>
+		</select>
+	</p>
+
+	<?php endif; ?>
